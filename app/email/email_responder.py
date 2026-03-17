b25adb5ca79467b93cabc0b28dd32226b7e8d979
Ibtesham Akhtar <ibteshamakhtar1@gmail.com>
2026-03-17 18:07:53 +0530

    stage 1 email and user chat response

--- app/email/email_responder.py
+++ app/email/email_responder.py
@@ -1,3 +1,5 @@
+import re
+
 from app.rag.rag_pipeline import RAGPipeline
 from app.llm.llm_client import LLMClient
 from app.llm.prompt_builder import build_email_prompt
@@ -11,19 +13,66 @@ class EmailResponder:
         self.rag = RAGPipeline(user_id)
         self.llm = LLMClient()
 
-
     def clean_response(self, response):
 
-        # Remove prompt duplication
         if "Subject:" in response:
             response = response[response.index("Subject:"):]
 
         return response.strip()
 
+    def extract_order_id(self, text):
+
+        match = re.search(r"\b\d{5,}\b", text)
+
+        if match:
+            return match.group()
+
+        return None
+
+    def extract_email(self, text):
+
+        match = re.search(
+            r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}", text
+        )
+
+        if match:
+            return match.group()
+
+        return None
 
     def generate_reply(self, customer_email):
 
-        retrieved_context = self.rag.retrieve(customer_email)
+        order_id = self.extract_order_id(customer_email)
+        customer_email_id = self.extract_email(customer_email)
+
+        retrieved_context = []
+
+        # 1️⃣ Direct order search
+        if order_id:
+
+            for doc in self.rag.documents:
+
+                if order_id in doc["text"]:
+
+                    retrieved_context = [doc["text"]]
+                    break
+
+        # 2️⃣ Email search
+        if not retrieved_context and customer_email_id:
+
+            for doc in self.rag.documents:
+
+                if customer_email_id in doc["text"]:
+
+                    retrieved_context = [doc["text"]]
+                    break
+
+        # 3️⃣ fallback vector search
+        if not retrieved_context:
+
+            retrieved_context = self.rag.retrieve(customer_email)
+
+        print("Retrieved Context:", retrieved_context)
 
         prompt = build_email_prompt(customer_email, retrieved_context)
 
